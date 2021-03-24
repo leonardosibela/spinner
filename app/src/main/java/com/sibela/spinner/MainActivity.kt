03649079d8b0af817094dc378f4ad0620acbeaaa
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-24 06:57:35 -0300

    Adding viewBinding

--- app/src/main/java/com/sibela/spinner/MainActivity.kt
+++ app/src/main/java/com/sibela/spinner/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.sibela.spinner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sibela.spinner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
